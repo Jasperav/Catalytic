7c82b5fa1daad61cfac134b741871aff16dca184
Jasperav <Jasperav@hotmail.com>
2022-12-15 22:37:53 +0100

    Update scylla to 0.7.0 (#29)

--- catalytic_table_to_struct/example/src/generated/mod.rs
+++ catalytic_table_to_struct/example/src/generated/mod.rs
@@ -12,8 +12,5 @@ pub use person::{Person, PersonRef};
 pub mod test_table;
 pub use test_table::{TestTable, TestTableRef};
 #[allow(dead_code, clippy::clone_on_copy)]
-pub mod uuidtable;
-pub use uuidtable::{Uuidtable, UuidtableRef};
-#[allow(dead_code, clippy::clone_on_copy)]
 pub mod person_by_email;
 pub use person_by_email::{PersonByEmail, PersonByEmailRef};
